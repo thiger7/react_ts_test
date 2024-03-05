a95aee82866a14625595c459ed555352ed5ad4d3
thiger7 <adf0423@gmail.com>
2024-03-05 09:09:40 +0900

    jsonplaceholderで取得するところまで

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { UserCard } from "./components/UserCard";
 import "./styles.css";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 const user = {
   id: 1,
@@ -9,8 +13,26 @@ const user = {
 };
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      });
+  };
   return (
     <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
       <UserCard user={user} />
     </div>
   );
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -7,8 +7,16 @@ type Props = {
 
 export const UserCard: VFC<Props> = (props) => {
   const { user } = props;
+
+  const style = {
+    border: "solid 1px #ccc",
+    borderRadius: "8px",
+    padding: "0 16px",
+    margin: "8px",
+  };
+
   return (
-    <div>
+    <div style={style}>
       <dl>
         <dt>名前</dt>
         <dd>{user.name}</dd>
